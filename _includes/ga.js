99e004b0df7ed7817de61c2c2413c0652c47c11f
0-k <martin.t.klein@gmail.com>
2021-09-16 22:24:52 +0200

    adding first version of cookie consent

---
+++ _includes/ga.js
@@ -0,0 +1,18 @@
+/*This function will load script and call the callback once the script has loaded*/
+function loadScriptAsync(scriptSrc, callback) {
+    if (typeof callback !== 'function') {
+        throw new Error('Not a valid callback for async script load');
+    }
+    var script = document.createElement('script');
+    script.onload = callback;
+    script.src = scriptSrc;
+    document.head.appendChild(script);
+}
+
+/* This is the part where you call the above defined function and "call back" your code which gets executed after the script has loaded */
+loadScriptAsync('https://www.googletagmanager.com/gtag/js?id={UA-207131579-1}', function(){
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '{UA-207131579-1}', { 'anonymize_ip': true });
+})
